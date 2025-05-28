f8a0bc60757d18c36bd833714ca2151fc9e4ca01
Genoss21 <fritz21.tobes@gmail.com>
2025-05-28 09:11:29 +0800

    Refactor base page structure: move BasePage to com.base package and update imports across the project

--- Selenium/demo/src/main/java/utilities/Utility.java
+++ Selenium/demo/src/main/java/utilities/Utility.java
@@ -2,7 +2,7 @@ package utilities;
 
 import org.openqa.selenium.WebDriver;
 
-import com.saucedemo.pages.BasePage;
+import com.base.BasePage;
 
 public class Utility {
 
@@ -11,8 +11,4 @@ public class Utility {
     public static void setUtilityDriver(){
         driver = BasePage.driver;
     }
-
-    //Generate a random number
-
-    //Return String to upper case
 }
