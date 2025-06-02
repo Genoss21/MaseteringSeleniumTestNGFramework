f11d8cff59036bcc13caa318cb9328db64190d5f
Genoss21 <fritz21.tobes@gmail.com>
2025-06-02 13:34:23 +0800

    Add LinksPage and LinksTest; implement methods for link interactions and validation

--- Selenium/demo/src/main/java/com/demoqa/pages/elements/ElementsPage.java
+++ Selenium/demo/src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -7,10 +7,16 @@ import com.demoqa.pages.HomePage;
 public class ElementsPage extends HomePage {
 
     private By webTalbesMenuItem = By.xpath("//li[@id='item-3']//span[text()='Web Tables']");
+    private By linksMenuItem = By.xpath("//li[@id='item-5']/span[text()='Links']");
 
     //method 
     public WebTablesPage clickWebTables() {
         click(webTalbesMenuItem);
         return new WebTablesPage();
+    }
+
+    public LinksPage clickLinks() {
+        click(linksMenuItem);
+        return new LinksPage();
     }
 }
