c11d371bf67f40e78d7eb2fef7c9eda189e51545
Genoss21 <fritz21.tobes@gmail.com>
2025-06-05 15:01:13 +0800

    Add PracticeFormPage methods for hobby selection and CheckboxTest for validation

--- Selenium/demo/src/main/java/com/demoqa/pages/Forms/PracticeFormPage.java
+++ Selenium/demo/src/main/java/com/demoqa/pages/Forms/PracticeFormPage.java
@@ -8,14 +8,55 @@ import org.openqa.selenium.By;
 public class PracticeFormPage extends FormsPage{
 
     private By femaleRadioButton = By.id("gender-radio-2");
+     private By sportsHobbyCheckbox = By.id("hobbies-checkbox-1");
+    private By readingHobbyCheckbox = By.id("hobbies-checkbox-2");
+    private By musicHobbyCheckbox = By.id("hobbies-checkbox-3");
 
-
+    //method to select a radio button
     public void clickFemaleRadioButton(){
         scrollToElementJS(femaleRadioButton);
         clickJS(femaleRadioButton);
     }
 
+    //method to check if the rado button is selected
     public boolean isFemaleRadioButtonSelected() {
         return find(femaleRadioButton).isSelected();
+    }
+
+    //method to select sports hobby checkbox
+    public void clickSportsCheckbox() {
+        if (!find(sportsHobbyCheckbox).isSelected()) {
+            scrollToElementJS(sportsHobbyCheckbox);
+            clickJS(sportsHobbyCheckbox);
+        }
+    }
+
+    //method to select reading hobby checkbox
+    public void clickReadingCheckbox() {
+        if (!find(readingHobbyCheckbox).isSelected()) {
+            scrollToElementJS(readingHobbyCheckbox);
+            clickJS(readingHobbyCheckbox);
+        }
+    }
+
+    //method to select music hobby checkbox
+    public void clickMusicCheckbox() {
+        if (!find(musicHobbyCheckbox).isSelected()) {
+            scrollToElementJS(musicHobbyCheckbox);
+            clickJS(musicHobbyCheckbox);
+        }
+    }
+
+    //method to unselect reading hobby checkbox
+    public void unclickReadingCheckbox() {
+        if (find(readingHobbyCheckbox).isSelected()) {
+            scrollToElementJS(readingHobbyCheckbox);
+            clickJS(readingHobbyCheckbox);
+        }
+    }
+    
+    //checks if the reading hobby checkbox is not selected 
+    public boolean isReadingSelected() {
+        return find(readingHobbyCheckbox).isSelected();
     }
 }
